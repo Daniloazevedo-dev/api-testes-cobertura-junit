225f2bdb97cbd31638c9014577c2ec0cabcb1c04
Danilo Azevedo <danilo.ann@gmail.com>
2022-07-30 12:06:44 -0300

    projeto iniciado

---
+++ src/main/java/br/com/cobertura/api/ApiTestesCoberturaJunitApplication.java
@@ -0,0 +1,13 @@
+package br.com.cobertura.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiTestesCoberturaJunitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiTestesCoberturaJunitApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/cobertura/api/ApiTestesCoberturaJunitApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.cobertura.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiTestesCoberturaJunitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

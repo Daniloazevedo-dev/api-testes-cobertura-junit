b7c7a9c2e28f15be4b1822a5866edb0fb4a9e35f
Danilo Azevedo <danilo.ann@gmail.com>
2022-08-02 19:32:56 -0300

    endponint findall para Usuario

--- src/main/java/br/com/cobertura/api/resource/UsuarioResource.java
+++ src/main/java/br/com/cobertura/api/resource/UsuarioResource.java
@@ -10,6 +10,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @RestController
 @RequestMapping(value = "/usuario")
 public class UsuarioResource {
@@ -23,6 +26,14 @@ public class UsuarioResource {
     @GetMapping(value = "/{id}")
     public ResponseEntity<UsuarioDTO> findById(@PathVariable Integer id) {
         return ResponseEntity.ok().body(mapper.map(usuarioService.findById(id), UsuarioDTO.class));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<UsuarioDTO>> findAll() {
+        return ResponseEntity.ok()
+                .body(usuarioService.findAll()
+                        .stream().map(x -> mapper
+                                .map(x,UsuarioDTO.class)).collect(Collectors.toList()));
     }
 
 }
--- src/main/java/br/com/cobertura/api/service/UsuarioService.java
+++ src/main/java/br/com/cobertura/api/service/UsuarioService.java
@@ -2,8 +2,12 @@ package br.com.cobertura.api.service;
 
 import br.com.cobertura.api.domain.Usuario;
 
+import java.util.List;
+
 public interface UsuarioService {
 
     Usuario findById(Integer id);
+
+    List<Usuario> findAll();
 
 }

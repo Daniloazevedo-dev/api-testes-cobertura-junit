d4fdf9e1d3b149fd15bbd34851186e14923dd30a
Danilo Azevedo <danilo.ann@gmail.com>
2022-08-13 18:32:23 -0300

    removendo anotações não utilizadas

--- src/main/java/br/com/cobertura/api/domain/dto/UsuarioDTO.java
+++ src/main/java/br/com/cobertura/api/domain/dto/UsuarioDTO.java
@@ -3,13 +3,11 @@ package br.com.cobertura.api.domain.dto;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
 @AllArgsConstructor
-@NoArgsConstructor
 public class UsuarioDTO {
 
     private Integer id;
